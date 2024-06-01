32e8486d7afc4c9c293ab37c3c60228f6afe982f
darkcharl <darkcharl@void.com>
2024-05-31 20:00:50 -0400

    chore: update uuid script

--- prep/get_uuids.sh
+++ prep/get_uuids.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-grep -i root ../PAK/Public/SecretScrolls5eSpells/Stats/Generated/Data/Object_SecretScrolls5eSpells.txt | awk '{ print $3 "," }'
+sed -En 's/^(new entry "[^"]+"|data "RootTemplate" "[^"]+"|^$)/\1/p' ../PAK/Public/SecretScrolls5eSpells/Stats/Generated/Data/Object_SecretScrolls5eSpells.txt  | tr -d '"' | awk '{ print $NF }'
+
